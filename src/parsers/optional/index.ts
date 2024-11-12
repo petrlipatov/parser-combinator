81cd888620e7c17901890235c0b97e7af87276bf
Petr Lipatov <lipatov3@gmail.com>
2024-11-12 23:25:00 +0300

    chore: Index files added to parsers

--- src/parsers/optional/index.ts
+++ src/parsers/optional/index.ts
@@ -1,9 +1,1 @@
-import { Parser, ParserOptions } from "../../types";
-import { repeat } from "../repeat";
-
-export function optional<T = unknown, R = unknown>(
-  parser: Parser,
-  options?: ParserOptions<T[]>
-): Parser {
-  return repeat(parser, { min: 0, max: 1, ...options });
-}
+export { optional } from "./optional";
